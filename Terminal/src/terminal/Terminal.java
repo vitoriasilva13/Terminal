9cf9722d20f232e6e53987883cfa49f80ba61e53
Vitória Silva <silva.lopes.vitoria@gmail.com>
2025-02-28 09:17:44 -0300

    Mudanças

--- Terminal/src/terminal/Terminal.java
+++ Terminal/src/terminal/Terminal.java
@@ -6,12 +6,14 @@ public class Terminal {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		CommandHandler commandHandler = new CommandHandler();
 		
+		System.out.println("Terminal Iniciado...");
 		while (true) {
             System.out.print("> ");
             String input = scanner.nextLine().trim();
             if (input.equals("exit")) break;
-            //commandHandler.executeCommand(input);
+            commandHandler.execute(input);
         }
 		scanner.close();
         System.out.println("Terminal encerrado.");
